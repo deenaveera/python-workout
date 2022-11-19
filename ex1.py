485d9c0657329c6640e308761cc491ee6d4ffdb5
Deena <dthayalan333@gmail.com>
2022-11-19 20:49:52 +0530

    added both files

---
+++ ex1.py
@@ -0,0 +1,4 @@
+
+print ("Welcome to python training.")
+
+print ( 1 + 5)
---
+++ ex2.py
@@ -0,0 +1,15 @@
+#Arithmentic operations
+
+print ("Arithmentic operations:")
+
+#Add
+print ( 5 + 5)
+#sub
+print( 10 - 5)
+#multi
+print (5 * 5)
+#div
+print (10 / 5)
+
+#reminder
+print (10 % 5)
